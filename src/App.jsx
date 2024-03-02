928c1cb76c8fd72aaf37b02f9c84e860ad1a9d43
AhaD <abuahad35188@gmail.com>
2024-03-02 16:04:27 +0600

    page setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+import Home from './Components/Home/Home'
+
+function App() { 
+  return (
+    <>
+      <h1 className="text-orange-600 text-center my-7 text-3xl">hello bangladesh</h1>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
